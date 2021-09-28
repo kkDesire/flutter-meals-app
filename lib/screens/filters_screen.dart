db5c6d4c5f3ee3976cac6eac6a6d53910b730fa4
kkDesire <kouamdesire90@gmail.com>
2021-09-28 13:27:04 +0000

    Reolacing page Instead of Pushing

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/main_drawer.dart';
+
 class FiltersScreen extends StatelessWidget {
   static final routeName = '/filters';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Filters'),
       ),
