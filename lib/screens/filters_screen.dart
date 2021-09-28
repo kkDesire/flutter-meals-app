f1f707dae14e9988d88816e26de5ef4d4bc3c5d2
kkDesire <kouamdesire90@gmail.com>
2021-09-28 20:14:00 +0000

    Adding filtering logic

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,14 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   static final routeName = '/filters';
+
+  final Map<String, bool> currentFilter;
+  final Function saveFliters;
+
+  FiltersScreen(this.currentFilter, this.saveFliters);
 
   @override
   _FiltersScreenState createState() => _FiltersScreenState();
@@ -16,6 +19,15 @@ class _FiltersScreenState extends State<FiltersScreen> {
   bool _vegetarian = false;
   bool _vegan = false;
   bool _lactoseFree = false;
+
+  @override
+  initState() {
+    _glutenFree = widget.currentFilter['gluten'] as bool;
+    _lactoseFree = widget.currentFilter['lactose'] as bool;
+    _vegetarian = widget.currentFilter['vegetarian'] as bool;
+    _vegan = widget.currentFilter['vegan'] as bool;
+    super.initState();
+  }
 
   Widget _buildSwitchListTile(
     String title,
@@ -38,6 +50,20 @@ class _FiltersScreenState extends State<FiltersScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Filters'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final selectedFilters = {
+                'gluten': _glutenFree,
+                'lactose': _lactoseFree,
+                'vegan': _vegan,
+                'vegetarian': _vegetarian,
+              };
+              widget.saveFliters(selectedFilters);
+            },
+          ),
+        ],
       ),
       drawer: MainDrawer(),
       body: Column(
@@ -52,54 +78,41 @@ class _FiltersScreenState extends State<FiltersScreen> {
           Expanded(
             child: ListView(
               children: [
-                _buildSwitchListTile(
-                  'Gluten-free',
-                  'Only inglude gluten-free meals.',
-                  _glutenFree,
-                  (newValue){
-                    setState(
+                _buildSwitchListTile('Gluten-free',
+                    'Only inglude gluten-free meals.', _glutenFree, (newValue) {
+                  setState(
                     () {
                       _glutenFree = newValue;
                     },
                   );
-                  }
-                ),
+                }),
                 _buildSwitchListTile(
-                  'Lactose-free',
-                  'Only inglude lactose-free meals.',
-                  _lactoseFree,
-                  (newValue){
-                    setState(
+                    'Lactose-free',
+                    'Only inglude lactose-free meals.',
+                    _lactoseFree, (newValue) {
+                  setState(
                     () {
                       _lactoseFree = newValue;
                     },
                   );
-                  }
-                ),
+                }),
                 _buildSwitchListTile(
-                  'Vegetarian',
-                  'Only inglude vegetarian meals.',
-                  _vegetarian,
-                  (newValue){
-                    setState(
+                    'Vegetarian', 'Only inglude vegetarian meals.', _vegetarian,
+                    (newValue) {
+                  setState(
                     () {
                       _vegetarian = newValue;
                     },
                   );
-                  }
-                ),
+                }),
                 _buildSwitchListTile(
-                  'Vegan',
-                  'Only inglude vegan meals.',
-                  _vegan,
-                  (newValue){
-                    setState(
+                    'Vegan', 'Only inglude vegan meals.', _vegan, (newValue) {
+                  setState(
                     () {
                       _vegan = newValue;
                     },
                   );
-                  }
-                ),
+                }),
               ],
             ),
           )
